9abeafada62f49569fe7634b4e8d0cd6137a3968
BenidictClemente <benidictclemente0@gmail.com>
2025-03-29 15:48:48 +0800

    Update no_using_center.py

--- no_using_center.py
+++ no_using_center.py
@@ -1,3 +1,7 @@
+#Input a word or phrase.
+#enter the width you like.
+#print the world of phrases with the width selected.
+
 def custom_center(text, width):
     spaces = (width - len(text)) // 2  
     return ' ' * spaces + text + ' ' * spaces  
