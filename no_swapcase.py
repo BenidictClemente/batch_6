c2d42a98c9eb8e34da717d790ff04e2e281ff0dd
BenidictClemente <benidictclemente0@gmail.com>
2025-03-29 15:46:29 +0800

    Update no_swapcase.py

--- no_swapcase.py
+++ no_swapcase.py
@@ -1,7 +1,9 @@
+#input words or phrases.
+#print the swapcase version
+
 def swap_case(s):
     return ''.join(char.lower() if char.isupper() else char.upper() for char in s)
 
-# User inputs the word
 word = input("Enter a word or phrase: ")
-print(swap_case(word))  # Print the swapped case version
+print(swap_case(word))  
 
