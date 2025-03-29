6dc33782ee500b1078446d021a660f33cfd5b7c9
BenidictClemente <benidictclemente0@gmail.com>
2025-03-29 15:52:39 +0800

    Update remove_prefixes.py

--- remove_prefixes.py
+++ remove_prefixes.py
@@ -1,3 +1,6 @@
+#enter a word 
+# remove the prefixes that means "not"
+#print the word without the prefix
 def remove_prefix(string, prefix):
     if string.startswith(prefix):
         return string[len(prefix):]  
@@ -5,7 +8,7 @@ def remove_prefix(string, prefix):
 
 
 text = input("Enter a word that stars with In: ")
-prefix = "In"
+prefix = "In,un,"
 result = remove_prefix(text, prefix)
 print("Original String:", text)
 print("After Removing Prefix:", result)
