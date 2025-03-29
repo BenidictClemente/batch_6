356d6eedd9ba073e4737ec5a174ebd50ba3441e3
BenidictClemente <benidictclemente0@gmail.com>
2025-03-29 15:55:47 +0800

    Update without_using_title.py

--- without_using_title.py
+++ without_using_title.py
@@ -1,19 +1,4 @@
-def custom_title(text):
-    result = ""
-    capitalize = True  
-    
-    for char in text:
-        if char.isalpha():
-            if capitalize:
-                result += char.upper()
-            else:
-                result += char.lower()
-            capitalize = False  
-        else:
-            result += char 
-            capitalize = True  
-    
-    return result
-
-word = input("Enter a word or phrase: ")
-print(custom_title(word))
+#Input a word
+#print the word with title form
+word = input("Enter a word")
+print(word.capitalize())
